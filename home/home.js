c79e942cdfcaf283727fa6ab481fc10758ca2473
saritha-00 <linux.saritha@gmail.com>
2025-03-16 23:33:06 -0400

    Initial commit for webapp project

---
+++ home/home.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 1000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
